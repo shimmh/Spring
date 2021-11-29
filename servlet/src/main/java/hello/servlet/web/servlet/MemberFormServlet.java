deafadb9df8b786fd97e8da9305d93e84f78a275
shimmh <31092342+shimmh@users.noreply.github.com>
2021-11-30 02:06:18 +0900

    서블릿으로 회원관리 웹 애플리케이션 작성

---
+++ servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,46 @@
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 서블릿으로 회원 관리 어플리케이션 생성
+ * - return : form 화면
+ */
+@WebServlet(name="memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter w = response.getWriter();
+        w.write("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                "    username: <input type=\"text\" name=\"username\" />\n" +
+                "    age:      <input type=\"text\" name=\"age\" />\n" +
+                " <button type=\"submit\">전송</button>\n" + "</form>\n" +
+                "</body>\n" +
+                "</html>\n"
+        );
+
+    }
+}
