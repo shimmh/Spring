b9348038b0d647deae1e3b2365e530deac281f74
shimmh <31092342+shimmh@users.noreply.github.com>
2021-12-09 18:41:14 +0900

    HTTP 응답 - 정적 리소스, 뷰 템플릿

---
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,33 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1(){
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model){
+        model.addAttribute("data", "hello!!");
+        return "response/hello"; // @Controller + String(return) : 뷰의 논리적 이름
+    }
+
+    /**
+     * 요청 URL을 참고해서 논리 뷰 이름을 찾는다.
+     * 명시성이 너무 떨어져 권장하지 않음
+     */
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model){
+        model.addAttribute("data", "hello!!!");
+    }
+
+}
